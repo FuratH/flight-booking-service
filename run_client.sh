104ddf23dd73e3548f37afe5fbe73e9ed9a208e7
FuratH <hamdanfurat@gmail.com>
2024-11-12 23:44:36 +0100

    update

--- run_client.sh
+++ run_client.sh
@@ -9,14 +9,14 @@ USERNAME=$(whoami)
 
 # Run the k6 load test
 k6 run /flight-booking-service/k6/script.js -e target="$SUT_IP:$SERVICE_PORT" \
-    --out csv=client_results_${TIMESTAMP}.csv --summary-export client_summary_${TIMESTAMP}.json 
+    --out csv=client_results_${SERVICE_PORT}_${TIMESTAMP} --summary-export client_summary_${TIMESTAMP}.json 
 
 # Wait for the test to complete
 wait
 
 # Upload results to Google Cloud Storage
-gsutil cp client_results_${TIMESTAMP}.csv gs://$BUCKET_NAME/client_results_${TIMESTAMP}.csv
-gsutil cp client_summary_${TIMESTAMP}.json gs://$BUCKET_NAME/client_summary_${TIMESTAMP}.json
+gsutil cp client_results_${SERVICE_PORT}_${TIMESTAMP}.csv gs://$BUCKET_NAME/client_results_${SERVICE_PORT}_${TIMESTAMP} & gsutil cp client_summary_${SERVICE_PORT}_${TIMESTAMP}.json gs://$BUCKET_NAME/client_summary_${TIMESTAMP}.json
+
 
 # Indicate that the task is done
 touch /done
