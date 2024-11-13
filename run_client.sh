bc1d694411f596a57a30b58cc411181ec7c3ac63
FuratH <hamdanfurat@gmail.com>
2024-11-13 17:03:30 +0100

    update upload function

--- run_client.sh
+++ run_client.sh
@@ -9,13 +9,13 @@ USERNAME=$(whoami)
 
 # Run the k6 load test
 k6 run /flight-booking-service/k6/script.js -e target="$SUT_IP:$SERVICE_PORT" \
-    --out csv=client_results_${SERVICE_PORT}_${TIMESTAMP}.csv --summary-export client_summary_${SERVICE_PORT}_${TIMESTAMP}.json
+    --out csv=client_results_${SERVICE_PORT}.csv --summary-export client_summary_${SERVICE_PORT}.json
 
 # Wait for the test to complete
 wait
 
 
-gsutil cp client_results_${SERVICE_PORT}_${TIMESTAMP}.csv gs://duet-benchmarking-results/client_results_${SERVICE_PORT}_${TIMESTAMP}.csv & gsutil cp client_summary_${SERVICE_PORT}_${TIMESTAMP}.json gs://duet-benchmarking-results/client_summary_${SERVICE_PORT}_${TIMESTAMP}.json
+gsutil cp client_results_${SERVICE_PORT}.csv gs://duet-benchmarking-results/${TIMESTAMP}/client_results_${SERVICE_PORT}.csv & gsutil cp client_summary_${SERVICE_PORT}.json gs://duet-benchmarking-results/${TIMESTAMP}/client_summary_${SERVICE_PORT}.json
 
 
 
