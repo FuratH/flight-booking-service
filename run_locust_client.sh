4115ecdbe41de71f17c6f3cc8ceb0ce8a528816e
FuratH <hamdanfurat@gmail.com>
2024-11-23 12:06:56 +0100

    update run script

--- run_locust_client.sh
+++ run_locust_client.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Parameters passed from the orchestrating VM
 SUT_IP=$1
 SERVICE_PORT=$2
@@ -7,7 +8,12 @@ TIMESTAMP=$3
 BUCKET_NAME=$4
 USERNAME=$(whoami)
 
-locust -f /flight-booking-service/locust/benchmark.py --host $SUT_IP:$SERVICE_PORT --web-port $SERVICE_PORT
+locust -f /flight-booking-service/locust/benchmark.py --host=http://$SUT_IP:$SERVICE_PORT --headless -u 230 --run-time 10m --csv=client_results_${SERVICE_PORT} --html=client_results_${SERVICE_PORT}.html --spawn-rate=50
+
+wait
+
+
+gsutil cp ../client_results_${SERVICE_PORT}_stats.csv gs://duet-benchmarking-results/${TIMESTAMP}/client_results_${SERVICE_PORT}_stats.csv & gsutil cp ../client_results_${SERVICE_PORT}.html gs://duet-benchmarking-results/${TIMESTAMP}/client_results_${SERVICE_PORT}.html
 
 
 
