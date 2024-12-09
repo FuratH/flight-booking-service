a86f089eb83021d59ffc5b60ebb6d629a407f071
FuratH <hamdanfurat@gmail.com>
2024-12-09 16:24:33 +0100

    Update run script

--- run_client.sh
+++ run_client.sh
@@ -24,7 +24,7 @@ gsutil cp client_results_${SERVICE_PORT}.csv gs://duet-benchmarking-results/${TI
 #sudo gcloud compute ssh hamdanfurat@sut --zone europe-west3-c -- \
  #   "gsutil cp '/tmp/cpu_usage.log' 'gs://duet-benchmarking-results/${TIMESTAMP}/cpu_usage.log'"
 
-sudo gcloud compute ssh hamdanfurat@sut --zone europe-west3-c -- \
+sudo gcloud compute ssh hamdanfurat@sut --zone europe-west1-c -- \
     "gsutil cp '/tmp/cpu_usage.log' 'gs://duet-benchmarking-results/${TIMESTAMP}/cpu_usage.log' & gsutil cp '/tmp/top_output.log' 'gs://duet-benchmarking-results/${TIMESTAMP}/top_output.log'"
 
 
